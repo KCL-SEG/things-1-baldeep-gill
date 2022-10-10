9f8b98c3aaa5369215073518bd96e44d35782b54
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2022-10-10 15:00:46 +0100

    Create custom model Things and define input validation

--- things/models.py
+++ things/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Things(models.Model):
+    name = models.CharField(
+        max_length = 30,
+        unique = True,
+        blank = False,
+    )
+    description = models.CharField(
+        max_length = 120,
+        unique = False,
+        blank = False,
+    )
+    quantity = models.IntegerField(
+        unique = True,
+        validators = [
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
